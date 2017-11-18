4eb1b536ea6e5c5014c4172553c59a27c0f072c7
Zegat <helpkrn@yandex.ru>
2017-11-18 19:14:51 +0300

    sum plus

--- Project1/src/math.java
+++ Project1/src/math.java
@@ -1,6 +1,11 @@
 public class math {
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+    int a =6;
+    int b =7;
+    int sum=a+b;
+
+    System.out.println("Ответ = "+sum);
     }
 }
